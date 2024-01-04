affd171f03bfeffc869e6dc053fa308fdef75309
huagechen <huage.chen@elastic.co>
2024-01-04 22:21:25 +0100

    fix full instrumentation

--- Chapter10/Elastiflix/full-intrumentation-with-rum/javascript-client/src/App.js
+++ Chapter10/Elastiflix/full-intrumentation-with-rum/javascript-client/src/App.js
@@ -3,11 +3,10 @@ import { BrowserRouter } from "react-router-dom";
 import { init as initApm } from '@elastic/apm-rum'
 
 export const apm = initApm({
-    serviceName: process.env.NODE_ENV === 'development' ?  "javascript-client-elastic-manual" : "${ELASTIC_APM_SERVICE_NAME}",
-    //serverUrl: 'https://fef53b05d7d8455699151da2edc8f280.apm.us-central1.gcp.cloud.es.io:443',
-    serverUrl: process.env.NODE_ENV === 'development' ?  process.env.REACT_APP_ELASTIC_APM_SERVER_URL : "${ELASTIC_APM_SERVER_URL}",
+    serviceName: "${ELASTIC_APM_SERVICE_NAME}",
+    serverUrl: "${ELASTIC_APM_SERVER_URL}",
     serviceVersion: '',
-    environment: process.env.NODE_ENV === 'development' ?  "dev" : "${ELASTIC_APM_ENVIRONMENT}",
+    environment: "${ELASTIC_APM_ENVIRONMENT}",
     distributedTracingOrigins: ['http://localhost:3000','http://localhost:3001','http://localhost:9000']
 })
 
