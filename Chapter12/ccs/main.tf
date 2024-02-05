72a407bacbbb9a35e40c4a32a6b2f24af81a9047
huagechen <huage.chen@elastic.co>
2024-02-05 22:16:52 +0100

    add ccs terraform example

---
+++ Chapter12/ccs/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    ec = {
+      source  = "elastic/ec"
+      version = "0.6.0"
+    }
+  }
+}
+
+provider "ec" {
+}
+
+data "ec_stack" "latest" {
+  version_regex = "latest"
+  region        = "gcp-europe-west3"
+}
+
+resource "ec_deployment" "custom-ccs-id" {
+  name                   = "new-team-deployment"
+
+  region                 = "gcp-europe-west3"
+  version                = data.ec_stack.latest.version
+  deployment_template_id = "gcp-storage-optimized"
+
+  elasticsearch = {
+    hot = {
+      size = "2g"
+      autoscaling = {
+        max_size = "8g"
+      }
+    }
+  }
+
+  kibana = {}
+  integrations_server = {}
+}
+
